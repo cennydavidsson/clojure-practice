0555bef0abd83795606e35eafa33e519b9893c33
Cenny Davidsson <cenny@me.com>
2023-09-17 13:27:38 +0200

    feat(exercise-2): counting the number of characters

---
+++ exercies_2/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to exercies_2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ exercies_2/src/exercies_2/core.clj
@@ -0,0 +1,11 @@
+(ns exercies-2.core
+  (:gen-class))
+
+(defn -main
+  "Counting the number of characters"
+  [& args]
+  (println "What is the input string?")
+  (flush)
+  (def input (read-line))
+  (printf "%1s has %2d characters" input (count input)) 
+)
