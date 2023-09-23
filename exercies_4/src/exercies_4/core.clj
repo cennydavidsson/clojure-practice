50fece6a34aeec81ec03e03a3fd769d9b728e1a2
Cenny Davidsson <cenny@me.com>
2023-09-23 23:34:07 +0200

    refactor(exercise-4): extract variable

--- exercies_4/src/exercies_4/core.clj
+++ exercies_4/src/exercies_4/core.clj
@@ -1,7 +1,7 @@
 (ns exercies-4.core
   (:gen-class))
 
-(defn prombt [word]
+(defn prombt-for [word]
   (printf "Enter a %s%n" word)
   (flush)
   (read-line))
@@ -10,9 +10,9 @@
 (defn -main
   "Mad-lib"
   [& args]
-  (def noun (prombt "noun"))
-  (def verb (prombt "verb"))
-  (def adjective (prombt "adjective"))
-  (def adverb (prombt "adverb"))
-  (printf "Do you %1s your %2s %3s %4s? That's hilarious!" verb adjective noun  adverb))
+  (let [noun (prombt-for "noun") 
+        verb (prombt-for "verb")
+        adjective (prombt-for "adjective")
+        adverb (prombt-for "adverb")]
+    (printf "Do you %1s your %2s %3s %4s? That's hilarious!" verb adjective noun adverb)))
 
