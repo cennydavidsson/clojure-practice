b2a185844a9a37c27a02c4859ad58e372f766899
Cenny Davidsson <cenny@me.com>
2023-09-24 20:58:38 +0200

    refactor(exercise-2): introduce variable

--- exercies_2/src/exercies_2/core.clj
+++ exercies_2/src/exercies_2/core.clj
@@ -6,8 +6,8 @@
   [& args]
   (println "What is the input string?")
   (flush)
-  (def input (read-line))
-  (if (empty? input)
-    (println "String can't be empty")
-    (printf "%1s has %2d characters" input (count input))))
+  (let [input (read-line)]
+    (if (empty? input)
+      (println "String can't be empty")
+      (printf "%s has %d characters" input (count input)))))
 
