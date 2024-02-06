cde1f5ca5c8439d33be6711932105a69a77970ac
Cenny Davidsson <cenny@me.com>
2024-02-06 11:19:03 +0100

    feat(exercise-14): case insensitive

--- exercise_14/src/exercise_14/core.clj
+++ exercise_14/src/exercise_14/core.clj
@@ -1,4 +1,5 @@
 (ns exercise-14.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
 (defn- exit! [message]
@@ -25,24 +26,27 @@
   (let [decimal (Math/pow 10 decimal)]
     (double (/ (int (* decimal value)) decimal))))
 
+(def tax-rate 1.055)
+
 (defn tax [amount state]
-  (if (= state "WI")
-    (let [tax-rate 1.055
-          total (round-up (* amount tax-rate) :decimal 2)
+  (if (= (str/lower-case state) "wi")
+    (let [total (round-up (* amount tax-rate) :decimal 2)
           tax (truncate (- total amount) :decimal 2)]
       {:total total :subtotal amount :tax tax})
     {:total amount}))
+
+(defn format-tax [result]
+  (str 
+    (when-let [subtotal (result :subtotal)]
+        (format "The subtotal is $%.2f\n" subtotal))
+    (when-let [tax (result :tax)]
+        (format "The tax is $%.2f\n" tax))
+    (format "The total is $%.2f" (result :total))))
 
 (defn -main
   "Tax Calculator"
   [& args]
   (let [amount (ask-number! "What is the order amount?")
-        state (ask-text! "What is the state?")
-        result (tax amount state)]
-    (println (str 
-               (when-let [subtotal (result :subtotal)]
-                 (format "The subtotal is $%.2f\n" subtotal))
-               (when-let [tax (result :tax)]
-                 (format "The tax is $%.2f\n" tax))
-               (format "The total is $%.2f" (result :total))))))
+        state (ask-text! "What is the state?")]
+    (println (format-tax (tax amount state)))))
 
--- exercise_14/test/exercise_14/core_test.clj
+++ exercise_14/test/exercise_14/core_test.clj
@@ -3,8 +3,18 @@
             [exercise-14.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
+  (testing "Tax calculator"
     (is (= {:total 10.0} 
            (tax 10.0 "MN")))
     (is (= {:total 10.55 :subtotal 10.0 :tax 0.55} 
-           (tax 10.0 "WI")))))
+           (tax 10.0 "WI")))
+    (is (= {:total 10.55 :subtotal 10.0 :tax 0.55} 
+           (tax 10.0 "Wi"))))
+  (testing "tax formater"
+    (is (= 
+          "The subtotal is $10.00\nThe tax is $0.55\nThe total is $10.55"
+          (format-tax {:total 10.55 :subtotal 10.0 :tax 0.55})))
+    (is (= 
+          "The total is $10.55"
+          (format-tax {:total 10.55})))))
+
